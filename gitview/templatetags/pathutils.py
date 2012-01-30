b7d2c4af67fef17f09ecb1d955364a9fca01b84d
n3wtron <n3wtron@gmail.com>
2012-01-30 18:44:30 +0100

    adjust path navigationt

--- gitview/templatetags/pathutils.py
+++ gitview/templatetags/pathutils.py
@@ -1,12 +1,22 @@
 from django import template
-from django.core.urlresolvers import reverse
-from django.conf import settings
+from os import sep
 register = template.Library()
 
 @register.filter(name='relpath')
 def relpath(fullPath,basePath):
     newPath= fullPath.replace('//','/').replace(basePath,'')
-    if newPath[0]=='/':
-        return newPath[1:]
-    else:
-        return newPath
+    return newPath
+    
+@register.filter(name='parent')
+def parent(fullPath,basePath):
+    subdir=relpath(fullPath,basePath)
+    if subdir[-1]==sep:
+        subdir=subdir[:-1]
+    dir_list =subdir.split(sep)
+    if len(dir_list)==1:
+        return ""
+    new_dir_list=dir_list[:-1];
+    newPath=""
+    for dr in new_dir_list:
+        newPath+=dr+sep
+    return newPath
