455b3a107c503880e42155c18f8e12473abfbbb7
n3wtron <n3wtron@gmail.com>
2012-03-15 17:00:18 +0100

    new Feature: ISSUE Pattern Detection Link

---
+++ gitview/templatetags/linkutils.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django import template
+register = template.Library()
+import re
+@register.filter(name='issueLink')
+def issueLink(message):
+    try:
+        pattern = settings.MESSAGE_ID_PATTERN.replace("%ID%",'([0-9]*)')
+        link = settings.ISSUE_URL
+        gr = re.search(pattern,message)
+        link = link.replace('%ID%',gr.group(1))
+        return '<a href="'+link+'">'+message+'</a>'
+    except AttributeError:
+        return message
+
+    
+
