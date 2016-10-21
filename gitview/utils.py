27caa53ea4bfcdec49f8cb341014a02a9c736939
Andrea Vanzan <vanzan.andrea@gmail.com>
2016-10-21 16:45:03 +0200

    fixed user header

--- gitview/utils.py
+++ gitview/utils.py
@@ -1,5 +1,4 @@
 import os
-from base64 import b64decode
 
 import settings
 
@@ -8,19 +7,18 @@ def checkRepoAuthorization(request, reposPath=None):
 	if settings.GITOLITE_EXEC_PATH is None or settings.GITOLITE_EXEC_PATH == "":
 		return True
 	try:
-		auth_header = request.META['HTTP_AUTHORIZATION'].replace("Basic ", "")
-		decoded = b64decode(auth_header)
-		user = decoded[0:decoded.index(':')]
+		user = request.META['REMOTE_USER']
 		if reposPath is None:
 			reposPath = request.GET['path'].replace("//", "/")
+		reposPath = reposPath[:reposPath.rindex('.git')]
 		if settings.GITOLITE_HOME is None or settings.GITOLITE_HOME == "":
 			gitolite_home = os.environ['HOME']
 		else:
 			gitolite_home = settings.GITOLITE_HOME
 		cmd = "export HOME=" + gitolite_home + ";" + settings.GITOLITE_EXEC_PATH + " access -q " + reposPath + " "+user
-
 		if os.system(cmd) != 0:
 			return False
-	except:
+	except Exception as e:
+		print e
 		return False
 	return True
